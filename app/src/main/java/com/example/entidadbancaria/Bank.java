8883d1559b9c0bbe954d81e1c2aac1784dd42bc1
Guillem Vicente Juan <guillem.vicente.juan@gmail.com>
2024-11-16 11:44:17 +0100

    bank entity extended with multiple themes and resources

--- app/src/main/java/com/example/entidadbancaria/Bank.java
+++ app/src/main/java/com/example/entidadbancaria/Bank.java
@@ -4,7 +4,6 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 
-import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class Bank extends AppCompatActivity {
